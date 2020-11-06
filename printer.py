2af4842a359696132e1296eea8747036c241e714
Amirpouya <ap.janghorban@gmail.com>
2020-11-06 10:38:32 +0330

    a new function to say hi

--- printer.py
+++ printer.py
@@ -4,10 +4,13 @@ Created on Fri Nov  6 10:13:31 2020
 
 @author: mohsen
 """
-
+def hi(name):
+    print('Hi {}'.format(name))
+    return
 
 def hello_world():
     print('Hello world!')
     return
 
 hello_world()
+hi('Mohsen')
