1a01cb950cffe18880ce7fa6cc72ca1770c78f68
Mohsen Mehrani <m.mehrani14@gmail.com>
2020-11-06 10:21:42 +0330

    say hello func added

--- printer.py
+++ printer.py
@@ -5,9 +5,13 @@ Created on Fri Nov  6 10:13:31 2020
 @author: mohsen
 """
 
+def say_hello_to(name):
+    print( 'Hi {} !'.format(name) )
+    return
 
 def hello_world():
     print('Hello world!')
     return
 
 hello_world()
+say_hello_to('Saghi')
